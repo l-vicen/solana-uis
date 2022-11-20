5fe8477017b6cca474cba8c480668362f21ed0f9
Lucas Vicentim <lucasvicentim0301@gmail.com>
2022-11-20 05:15:18 +0100

    DEV: customer_page

--- customer.py
+++ customer.py
@@ -2,5 +2,6 @@ import db as db
 import streamlit as st
 
 def display_customer_dashboard():
+    st.title('Customer Credit Score')
     df = db.getDataSheetCS()
     st.table(df)
--- db.py
+++ db.py
@@ -1,8 +1,8 @@
-# Dependencies
 import streamlit as st
 import pandas as pd
 
 from google.oauth2 import service_account
+from gspread_dataframe import get_as_dataframe
 from gsheetsdb import connect
 import gspread
 
@@ -17,23 +17,23 @@ conn = connect(credentials=credentials)
 
 # Perform SQL query on the Google Sheet.
 # Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
+# @st.cache(ttl=600)
+# def run_query(query):
+#     rows = conn.execute(query, headers=1)
+#     rows = rows.fetchall()
+#     return rows
 
-def getDataSOL(dataframe):
-    pass
+# def getDataSheetCS():
+#     gsheet_url = "https://docs.google.com/spreadsheets/d/1TscPz0hQe8PbnS3gfssrYM21K_n4N1RR4kxAnWHjodc/edit?usp=sharing"
+#     rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
+#     df_gsheet = pd.DataFrame(rows)
+#     return df_gsheet
 
 def getDataSheetCS():
-    pass
-
-def getDataSheetCS():
-    gsheet_url = "https://docs.google.com/spreadsheets/d/1TscPz0hQe8PbnS3gfssrYM21K_n4N1RR4kxAnWHjodc/edit?usp=sharing"
-    conn = connect()
-    rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
-    df_gsheet = pd.DataFrame(rows)
-    return df_gsheet
+    sa = gspread.service_account("credentials.json")
+    sh = sa.open("hackaTUM")
+    worksheet = sh.get_worksheet(1)
+    df_read = get_as_dataframe(worksheet, usecols=[0,1], nrows=20)
+    return df_read
 
     
