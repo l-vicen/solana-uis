6c9338dc19d727d298c0fabd6b3a7f67f37b42f9
Lucas Vicentim <lucasvicentim0301@gmail.com>
2022-11-20 06:53:07 +0100

    DEV: merchant

--- db.py
+++ db.py
@@ -15,25 +15,18 @@ credentials = service_account.Credentials.from_service_account_info(
 )
 conn = connect(credentials=credentials)
 
-# Perform SQL query on the Google Sheet.
-# Uses st.cache to only rerun when the query changes or after 10 min.
-# @st.cache(ttl=600)
-# def run_query(query):
-#     rows = conn.execute(query, headers=1)
-#     rows = rows.fetchall()
-#     return rows
-
-# def getDataSheetCS():
-#     gsheet_url = "https://docs.google.com/spreadsheets/d/1TscPz0hQe8PbnS3gfssrYM21K_n4N1RR4kxAnWHjodc/edit?usp=sharing"
-#     rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
-#     df_gsheet = pd.DataFrame(rows)
-#     return df_gsheet
-
 def getDataSheetCS():
     sa = gspread.service_account("credentials.json")
     sh = sa.open("hackaTUM")
     worksheet = sh.get_worksheet(1)
     df_read = get_as_dataframe(worksheet, usecols=[0,1], nrows=20)
+    return df_read
+
+def getDataSheetMA():
+    sa = gspread.service_account("credentials.json")
+    sh = sa.open("hackaTUM")
+    worksheet = sh.get_worksheet(2)
+    df_read = get_as_dataframe(worksheet, usecols=[0,1,2,3], nrows=9)
     return df_read
 
     
--- merchant.py
+++ merchant.py
@@ -1,2 +1,44 @@
+import streamlit as st
+import pandas as pd
+import db as db
+import plotly.express as px
+from datetime import datetime
+
 def display_merchant_dashboard():
-    pass
+    st.title('Merchant Analytics')
+    df = db.getDataSheetMA()
+    st.write('---')
+    st.write('## Dashboard')
+    st.table(df)
+    st.write('---')
+    st.write('## Analytics')
+    st.info("KPIs that might incentivize the Merchant to adopt Solana-Pay could be: Total Sales over Time, Preferred Goods, Number of Solana-Pay Clients etc ...")
+    col1, col2 = st.columns([2,1])
+
+    df["Products"] = df["Products"].astype(object)
+    product = df["Products"].tolist()
+    # st.write(product)
+
+    # Edge case testing would be to provide a transaction with no product reference.
+    count = []
+    for t in range(len(product)):
+        sum = 0
+        s = product[t]
+        for i in range(0, len(s)):
+            if s[i] == " ":
+                sum += 1
+        count.append(sum + 1)
+    # st.write(count)
+
+    timestamps = df["Timestamp"].tolist()
+    timestamps = [datetime.utcfromtimestamp(t).strftime('%Y-%m-%d %H:%M:%S') for t in timestamps]
+    # st.write(timestamps)
+    f = px.line(timestamps, count)
+    col1.subheader("Total Sales over Time")
+    col1.plotly_chart(f)
+
+    count = df["Solana_Payment"].value_counts()
+    types = list(set(df["Solana_Payment"].tolist()))
+    fig = px.pie(values = count, names = types)
+    col2.subheader("Solana-Pay Users")
+    col2.plotly_chart(fig)
