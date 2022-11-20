b7ec92c42b2a46be7ac31e626dd818f513013780
Lucas Vicentim <lucasvicentim0301@gmail.com>
2022-11-20 08:04:41 +0100

    DEV: final

--- home.py
+++ home.py
@@ -10,14 +10,14 @@ def display_home():
     col1.header('About')
     col1.info('Our approach to the @hackTUM Challenge is to work on the incentives of the agents participating in a Solana Pay Transaction. We believe one way to approach this incentive dilemma is by providing the Customer with one way to keep track of his Credit Score and providing Merchants the opportunity to have analytics of products & transaction-related data. Least but not least, the Loaner is incentivized by having the opportunity to allocate his capital with more information and sometime security.')
 
-    col2.header('App Features')
+    col2.header('Challenge Results')
     col2.info(
         """
-        * Solana Pay: On-chain Referecing
-        * Helius-Listener Integration:
+        * [Solana Pay: On-chain Referecing](https://github.com/l-vicen/solana-pay)
+        * [Helius-Listener Integration](https://l-vicen-helius-listener-listener-7y4921.streamlit.app/):
             * Credit Scoring Mechanism
-            * Analytics Aggregador (Sheets)
-        * Agents UIs: 
+            * Analytics Aggregator (Sheets)
+        * [Agents UIs](https://l-vicen-solana-uis-app-ezy84t.streamlit.app/)
             * Customer
             * Merchant
             * Loaner
